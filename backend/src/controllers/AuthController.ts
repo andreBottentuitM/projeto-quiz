2a3b65f492d2c5a38c968287c797a36ef85c09ba
andre_bolsistasoftex@procenge.com.br <andre.bottentuit@gmail.com>
2023-04-21 21:08:41 -0300

    Feat:Desenvolvendo criptografia e token.

--- backend/src/controllers/AuthController.ts
+++ backend/src/controllers/AuthController.ts
@@ -1,15 +1,29 @@
 import {matchedData, validationResult} from 'express-validator'
+import bcrypt from "bcrypt"
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv'
+dotenv.config();
 
 export const signup = async (req:any, res:any) => {
-    const errors = validationResult(req)
+  const errors = validationResult(req)
+  
+  if(!errors.isEmpty()){
+    res.json({error: errors.mapped()})
+    return
+  }
+  
+  const data = matchedData(req)
+      
+  const passwordHash = await bcrypt.hash(data.passwordRegister, 10)
 
-    if(!errors.isEmpty()){
-        res.json({error: errors.mapped()})
-        return
-      }
+  const newUser = {
+   name:data.nameRegister,
+   email: data.emailRegister,
+   password: passwordHash
+  }
 
-      const data = matchedData(req)
-      console.log(data)
+  generateTokenReponse(newUser)
+    
 }
 
 export const signin = async (req:any, res:any) => {
@@ -20,5 +34,19 @@ export const signin = async (req:any, res:any) => {
         }
         const data = matchedData(req)
 
-        console.log(data)
+}
+
+const generateTokenReponse = (newUser:any) => {
+
+   const token = jwt.sign(
+     {email:newUser.email},
+     process.env.JWT_SECRET!,{
+     expiresIn:"30d"
+   });
+
+    return {
+      name: newUser.name,
+      email: newUser.email,
+      token: token
+    };
 }
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,8 @@ import express from 'express'
 import cors from 'cors'
 import userRouter from './routes';
 import path from 'path';
+import dotenv from 'dotenv'
+dotenv.config();
 
 const app = express()
 app.use(express.json());
@@ -14,7 +16,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname,'public', 'index.html'))
 })
 
-const port = 5000
+const port = process.env.PORT || 5000;
 
 app.listen(port, ()=> {
     console.log('Website served on http://localhost:' + port)
