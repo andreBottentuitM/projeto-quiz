2175637510ab7ce480af69872a57aa0d0d9a293c
andre_bolsistasoftex@procenge.com.br <andre.bottentuit@gmail.com>
2023-04-28 22:32:19 -0300

    Feat:Desenvolvendo Upload Files.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -13,7 +13,7 @@ app.use(fileupload())
 
 app.use("/api", userRouter);
 
-app.use(express.static(__dirname+'public'));
+app.use(express.static(__dirname+'/public'));
 
 
 const port = process.env.PORT || 5000;
